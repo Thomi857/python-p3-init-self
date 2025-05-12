cc5fc1180004c93a73eaa4d503c7b534e06b6c66
CliffLinusthomi <linusthomi2@gmail.com>
2025-05-12 11:31:47 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name,breed="Mutt"):
+        self.breed = breed
+        self.name = name
+
+    # dog.__init__(name, breed = "Mutt")
+    #     self.breed = breed
+    #     self.name = name
+
+    def bark(self):
+        print("Woof!")
+
+fido = Dog("Fido")
+fido.name
+# Fido
+
+snoopy = Dog("Snoopy")
+snoopy.name
+# Snoopy
+
+
+
